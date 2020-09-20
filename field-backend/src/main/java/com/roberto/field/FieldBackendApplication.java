0bb761d64a694db8ff58d0208780abd38328628d
Roberto Rocha <rrocha.roberto@gmail.com>
2020-09-20 14:16:21 -0300

    Base Spring Boot project (#3)

---
+++ field-backend/src/main/java/com/roberto/field/FieldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldBackendApplication.class, args);
+	}
+
+}
---
+++ field-backend/src/test/java/com/roberto/field/FieldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.field;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
